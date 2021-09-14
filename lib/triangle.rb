72c2cdaddec568637b38507423041fc3d4ba3c95
Kyle Ortiz <kyleaortiz@gmail.com>
2021-09-13 19:53:27 -0500

    commit tree 2f8b3579f75a0a3d89750855b16dc6d4989c5e30

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,41 @@
 class Triangle
-  # write code here
+  attr_accessor :sides
+  
+  def initialize(side_1,side_2,side_3)
+    @sides = []
+    @sides.push(side_1)
+    @sides.push(side_2)
+    @sides.push(side_3)
+  end
+
+  def tri_checker
+    index_2 = -1
+    index_3 = -2
+    previous_side = sides[index_2]
+    third_side = sides[index_3]
+    sides.each do |side|
+      if side <= 0 
+        raise TriangleError
+      elsif side + previous_side <= third_side
+        raise TriangleError
+      end
+      index_2 +=1
+      index_3 += 1
+    end
+  end
+
+  def kind 
+    tri_checker
+
+    if sides[0]==sides[1] && sides[1]==sides[2] 
+      :equilateral
+    elsif sides[0]==sides[1] || sides[1]==sides[2] || sides[2]==sides[0]
+      :isosceles
+    else
+      :scalene
+    end
+  end
+
+  class TriangleError < StandardError
+  end
 end
